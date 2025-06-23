eb05af1ebe7da43259f3849a129a38ea5e22d911
carbonaraissogood <fernandezmaxxinne14@gmail.com>
2025-06-23 16:37:27 +0800

    feat: implement AddToCart component with initial structure

---
+++ src/AddToCart.jsx
@@ -0,0 +1,37 @@
+function AddToCart() {
+
+  return (
+    <div>
+      <h2>CART</h2>
+      
+      {/* <table>
+        <thead>
+          <tr>
+            <th>Added Items</th>
+            <th>Description</th>
+            <th>Size</th>
+            <th>Color</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.id}>
+              <td>{item.img}</td>
+              <td>{item.description}</td>
+              <td>{item.size}</td>
+              <td>{item.color}</td>
+              <td>{item.price}</td>
+              <td>{item.quantity}</td>
+              <td>{item.total}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table> */}
+    </div>
+  );
+};
+
+export default AddToCart;
