63ae23cb2342378149c79d3c7da13d750e18b04c
carbonaraissogood <fernandezmaxxinne14@gmail.com>
2025-06-23 16:37:36 +0800

    feat: update App component to include LogInPage and SignUpPage imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import AddToCart from './AddToCart';
 import './App.css'
 import FakeStoreApp from './FakeStoreApp';
+import LogInPage from './LogInPage';
+import SignUpPage from './SignUpPage';
 
 function App() {
   
 
   return (
     <div>
-      <FakeStoreApp></FakeStoreApp>
+      {/* <FakeStoreApp></FakeStoreApp> */}
+      {/* <LogInPage></LogInPage> */}
+      {/* <SignUpPage></SignUpPage> */}
+      <AddToCart></AddToCart>
     </div>
   );
 };
