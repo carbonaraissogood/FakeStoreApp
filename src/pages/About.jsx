64ebd9dcf2b0a129d6e5b093e7ba549bde348297
carbonaraissogood <fernandezmaxxinne14@gmail.com>
2025-07-03 11:49:57 +0800

    setup the about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <main>
+      <h1>About Us</h1>
+
+      <p>
+        Welcome to Grand Bazaar — your one-stop online destination for everything you love!
+
+        <br /><br />
+
+        At Grand Bazaar, we believe that shopping should be simple, secure, and enjoyable. Whether you’re looking for the latest fashion, essential home goods, tech gadgets, or unique finds, we’re here to bring the best products right to your doorstep. Our goal is to connect you with quality items at competitive prices — all with just a few clicks.
+
+        <br /><br />
+
+        Founded in 2025, our journey began with a passion for innovation and a deep understanding of what customers truly value: convenience, trust, and excellent service. Since then, we’ve grown into a trusted online marketplace committed to delivering a seamless shopping experience, from browsing to checkout to fast delivery.
+      </p>
+    </main>
+  )
+}
+
+export default About
