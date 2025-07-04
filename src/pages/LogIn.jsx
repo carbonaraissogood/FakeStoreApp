453991a00d87a62bcedd65fcc3b1310bb83c12c5
carbonaraissogood <fernandezmaxxinne14@gmail.com>
2025-07-04 14:10:13 +0800

    setup the login page and navigate it to homepage

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,4 +1,45 @@
-const LogIn = () => {
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const LogIn = ({ users }) => {
+
+  const [usernameOrEmail, setUsernameOrEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleUsernameOrEmailChange = (event) => {
+    setUsernameOrEmail(event.target.value);
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const input = { 
+                    usernameOrEmail: usernameOrEmail,
+                    password: password
+                  };
+
+    const isUserExisting = users.find((user) => (
+      (user.username === input.usernameOrEmail
+        ||
+        user.email === input.usernameOrEmail
+      )
+      &&
+      (user.password === input.password)
+    ))
+    
+    if (!isUserExisting) {
+      alert('Oops! It seems your username/email or password is incorrect.');
+    } else {
+      alert("Login successfully!");
+      navigate("/");
+    }
+  }
 
   return (
     <main>
@@ -7,20 +48,27 @@ const LogIn = () => {
       <h2>Welcome Back</h2>
       <p>Please enter your details</p>
 
-      <input
-        type="email"
-        placeholder="Email address"
-      />
+      <form onSubmit={handleSubmit}>
 
-      <input
-        type="password"
-        placeholder="Password"
-      />
+        <input
+          type="text"
+          placeholder="Username or Email"
+          value={usernameOrEmail}
+          onChange={handleUsernameOrEmailChange}
+        />
 
-      <p><a href="">Forgot Password</a></p>
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
 
-      <button>Sign in</button>
-      <button>Sign in with Google</button>
+        <p><a href="">Forgot Password</a></p>
+
+        <button>Login</button>
+        
+      </form>
 
       <p>Don't have an account? <a href="">Sign up</a></p>
     </main>
