80ac79fc602be87dea3b73f6eba5eae4bf42a589
carbonaraissogood <fernandezmaxxinne14@gmail.com>
2025-07-04 14:11:20 +0800

    setup the signup page that accesses the link for users and navigated it to the login page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,44 @@
-const SignUp = () => {
+import { useState } from "react";
+import axios from "axios";
+
+const SignUp = ({ users }) => {
+
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  }
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const user = { 
+                    id: (users.length + 1),
+                    username: username,
+                    email: email,
+                    password: password
+                  };     
+
+    axios
+          .post('https://fakestoreapi.com/users', user)
+          .then(response => console.log(response.data));
+          
+    setUsers(prevUsers => [...prevUsers, user]);   
+
+    console.log(users);
+
+    alert("You created an account succesfully!");
+  }
 
   return (
     <main>
@@ -7,27 +47,38 @@ const SignUp = () => {
       <h2>Create your account</h2>
       <p>Get Started for free</p>
 
-      <input
-        type="email"
-        placeholder="Email address"
-      />
+      <form onSubmit={handleSubmit}>
 
-      <input
-        type="password"
-        placeholder="Password"
-      />
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
 
-      <p>Remember me</p>
+        <input
+          type="email"
+          placeholder="Email address"
+          value={email}
+          onChange={handleEmailChange}
+        />
 
-      <p><a href="">Forgot Password</a></p>
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
 
-      <button>Login</button>
+        <p>Remember me</p>
 
-      <p>or</p>
+        <p><a href="">Forgot Password</a></p>
 
-      <button>Continue with Google</button>
+        {/* Paano ko ililink to sa  Login na hindi nawawala yung data? */}
+        <button type="submit">Sign Up</button>
+        
+      </form>
 
-      <p>Don't have an account? <a href="">Sign up</a></p>
     </main>
   );
 };
