077fa7a07e7c1d9b087b0bd8ae2ffa6c5cd9e656
carbonaraissogood <fernandezmaxxinne14@gmail.com>
2025-07-17 17:07:54 +0800

    fix: Refactor NavigationBar component to use divs instead of ul for improved structure

--- src/pages/NavigationBar/Nav.jsx
+++ src/pages/NavigationBar/Nav.jsx
@@ -4,53 +4,73 @@ import styles from './Nav.module.css'
 const Nav = ({ title }) => {
   return (
     <nav>
-      <ul className={styles.navBar}>
 
-        <li
-          className={styles.storeName}
-        >
-          <Link to="/"><strong>{title}</strong></Link>
-        </li>
+      <div className={styles.notificationBarContainer}>
 
-        <li>
-          <Link to="/">Home</Link>
-        </li>
+        <div className={styles.textIcon}>
 
-        {/* Categories */}
-        <li>
-          <Link to="/shop">Shop</Link>
-        </li>
+          <div className={styles.icon}>
+            <img src="src\pages\NavigationBar\ticket-percent.svg" alt="" />
+          </div>
+          
+          <p>30% off storewide — Limited time! </p>
+        </div>
 
-        <li>
-          <Link to="/items">Product</Link>
-        </li>
+        <div className={styles.shopNow}>
+          <a href="">Shop Now</a>
+
+          <img src="src\pages\NavigationBar\arrow-right.svg" alt="" />
+        </div>
+
+      </div>
+
+      <div className={styles.navBar}>
+        <div className={styles.logo}>
+          <Link className={styles.storeName} to="/">{title}</Link>
+        </div>
+
+        <div className={styles.linkGroup}>
+
+          <div className={styles.link}>
+            <Link className={styles.linkName} to="/">Home</Link>
+          </div>
+
+          <div className={styles.link}>
+            <Link className={styles.linkName} to="/shop">Shop</Link>
+          </div>
+
+          <div className={styles.link}>
+            <Link className={styles.linkName} to="/items">Product</Link>
+          </div>
+
+          <div className={styles.link}>
+            <Link className={styles.linkName} to="/about">Contact Us</Link>
+          </div>
+
+        </div>
 
 
-        {/* <li>
-          <Link to="/signup">Sign Up</Link>
-        </li>
+        <div className={styles.icons}>
 
-        <li>
-          <Link to="/login">Login</Link>
-        </li> */}
+          <div className={styles.search}>
+            <img src="src\pages\NavigationBar\search 02.svg" alt="" />
+          </div>
 
-        <li>
-          <Link to="/about">Contact Us</Link>
-        </li>
+          <div className={styles.userPFP}>
+            <img src="src\pages\NavigationBar\user-circle.svg" alt="" />
+          </div>
 
-        <li>
-          Search
-        </li>
+          <div className={styles.cartButton}>
+            <div className={styles.shoppingBag}>
+              <img src="src\pages\NavigationBar\shopping bag.svg" alt="" />
+            </div>
+            <div className={styles.shoppingCartCounter}></div>
+          </div>
 
-        <li>
-          Profile
-        </li>
+        </div>
 
-        <li>
-          Basket
-        </li>
 
-      </ul>
+      </div>
     </nav>
   )
 }
