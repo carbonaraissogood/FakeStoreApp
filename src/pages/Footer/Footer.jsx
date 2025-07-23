09ae51a6d3280ceb35536636340aad48af94f52f
carbonaraissogood <fernandezmaxxinne14@gmail.com>
2025-07-23 17:47:41 +0800

    updated the UI of the footer

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,7 +1,42 @@
+import styles from './Footer.module.css';
+
 const Footer = () => {
   return (
-    <footer className='footerContainer'>
-      <p>&copy; 2025</p>
+    <footer className={styles.footerContainer}>
+      <div className={styles.topContainer}>
+
+        <div className={styles.leftSide}>
+          <p className={styles.name}>Grand Bazaar</p>
+          <p className={styles.line}>|</p>
+          <p className={styles.shopCategory}>Gift & Decoration Store</p>
+        </div>
+
+        <div className={styles.rightSide}>
+          <p>Home</p>
+          <p>Shop</p>
+          <p>Product</p>
+          <p>Blog</p>
+          <p>Contact Us</p>
+        </div>
+
+      </div>
+
+      <div className={styles.bottomContainer}>
+
+        <div className={styles.leftSide}>
+          <p>Copyright &copy; 2025 Grand Bazaar. All rights reserved</p>
+          <p className={styles.privPolicy}>Privacy Policy</p>
+          <p className={styles.terms}>Terms of Use</p>
+        </div>
+
+        <div className={styles.rightSide}>
+          <button><img src="src\pages\Footer\instagram.svg" alt="instagram icon" /></button>
+          <button><img src="src\pages\Footer\facebook.svg" alt="fb icon" /></button>
+          <button><img src="src\pages\Footer\youtube.svg" alt="yt icon" /></button>
+        </div>
+
+      </div>
+
     </footer>
   )
 }
