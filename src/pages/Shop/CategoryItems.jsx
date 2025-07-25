3ba047ba1e63d8b43a2417af1dc00aeec333c84a
carbonaraissogood <fernandezmaxxinne14@gmail.com>
2025-07-25 14:58:52 +0800

    added an event handler to prevent the default reloading of the page

--- src/pages/Shop/CategoryItems.jsx
+++ src/pages/Shop/CategoryItems.jsx
@@ -1,22 +1,21 @@
-import { Link } from "react-router-dom"
-import styles from './Category.module.css';
+import { Link } from "react-router-dom";
+import styles from './CategoryItems.module.css';
 
-const CategoryItems = ({ category, setSearch, search }) => {
+const CategoryItems = ({ category, setSearch }) => {
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setSearch(category.toLowerCase())
+  }
+
   return (
       <Link to="/items">
         
         <button
           className={styles.category}
-          onClick={() => setSearch(category.toLowerCase())}
-          style={{ backgroundColor: search === category.toLowerCase() ? '#e0e0e0' : 'white' }}
+          onClick={handleClick}
         >
-          <img 
-            src="https://as2.ftcdn.net/v2/jpg/03/64/41/07/1000_F_364410756_Ev3WoDfNyxO9c9n4tYIsU5YBQWAP3UF8.jpg"
-            alt=""
-          />
-
           {category}
-          
         </button>
       </Link>
   )
